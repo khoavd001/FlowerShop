fe509257b9fee820e5ee1e6cf0958744905ab997
tuankha2021 <79949413+tuankha2021@users.noreply.github.com>
2022-01-09 10:19:38 +0700

    Add files via upload

--- resources/views/page/ynghiahoa/HoaDongTien.blade.php
+++ resources/views/page/ynghiahoa/HoaDongTien.blade.php
@@ -7,42 +7,42 @@
 @section('phanloai')
 
     <h3>Ý nghĩa hoa đồng tiền</h3>
-    <div align="center"><iframe width="580" height="335" src="https://www.youtube.com/embed/m1ClHHGGLWI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+    <div align="center"><iframe width="580" height="335" src="https://www.youtube.com/embed/m1ClHHGGLWI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay ; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
     <div class="text-here">
-        <p>
+        <p class="p">
             <strong>  Thế Giới Hoa Tươi cùng bạn tham khảo ý nghĩa một số loài hoa với mong muốn bạn sẽ chọn được những bó hoa tươi ưng ý, phù hợp với từng dịp lễ và tình cảm của mình. Dù thế nào đi nữa người nhận sẽ hạnh phúc khi nhận được tình cảm chân thành của bạn muốn
                 nhờ những bông hoa thay lời nói.</strong></p>
-        <p>Loài hoa này tượng trưng cho sự hạnh phúc. Nó còn mang ý nghĩa về vẻ đẹp và điều kì diệu. Nó mang đến cho chúng ta sự tươi sáng và vui vẻ; không những thế, nó còn thể hiện sự ngây thơ, tình yêu và lòng ca ngợi. </p>
-        <p> Bạn có thể tìm thấy được sự trong trắng, tinh khiết nơi đồng tiền trắng, tình yêu ở những bông hoa màu đỏ, niềm hạnh phúc của hoa đồng tiền vàng hay sự ca ngợi, khích lệ nơi đồng tiền hồng. Có rất nhiều loài hoa có thể bày tỏ suy nghĩ và tình cảm của bạn nhưng hoa đồng tiền luôn nổi bật hẳn lên bởi sự rực rỡ đầy tươi vui của chúng. Khi bạn tặng cho ai đó những bông hoa này, chúng không những truyền đạt cho họ những thông điệp sâu sắc đầy ý nghĩa mà còn giúp đem lại một ấn tượng khó quên.</p>
-        <p>Hoa đồng tiền biểu trưng cho sự tin tưởng và sự sôi nổi</p>
-        <p>Nếu bạn muốn chinh phục trái tim của 1 cô gái cá tín, tự tin và sôi nổi, hãy tặng cô ấy 1 chậu hoa đông tiền. Những bông hoa màu hồng dễ thương vươn thẳng lên từ tán lá xanh thẫm thể hiện một cá tính độc lập nhưng vẫn dịu dàng, mềm mại. cánh hoa xoè rộng, màusắc tươi sáng thể hiện sức sống mạnh liệt, sư nhiệt tình tuyệt vời.</p>
+        <p class="p">Loài hoa này tượng trưng cho sự hạnh phúc. Nó còn mang ý nghĩa về vẻ đẹp và điều kì diệu. Nó mang đến cho chúng ta sự tươi sáng và vui vẻ; không những thế, nó còn thể hiện sự ngây thơ, tình yêu và lòng ca ngợi. </p>
+        <p class="p"> Bạn có thể tìm thấy được sự trong trắng, tinh khiết nơi đồng tiền trắng, tình yêu ở những bông hoa màu đỏ, niềm hạnh phúc của hoa đồng tiền vàng hay sự ca ngợi, khích lệ nơi đồng tiền hồng. Có rất nhiều loài hoa có thể bày tỏ suy nghĩ và tình cảm của bạn nhưng hoa đồng tiền luôn nổi bật hẳn lên bởi sự rực rỡ đầy tươi vui của chúng. Khi bạn tặng cho ai đó những bông hoa này, chúng không những truyền đạt cho họ những thông điệp sâu sắc đầy ý nghĩa mà còn giúp đem lại một ấn tượng khó quên.</p>
+        <p class="p">Hoa đồng tiền biểu trưng cho sự tin tưởng và sự sôi nổi</p>
+        <p class="p">Nếu bạn muốn chinh phục trái tim của 1 cô gái cá tín, tự tin và sôi nổi, hãy tặng cô ấy 1 chậu hoa đông tiền. Những bông hoa màu hồng dễ thương vươn thẳng lên từ tán lá xanh thẫm thể hiện một cá tính độc lập nhưng vẫn dịu dàng, mềm mại. cánh hoa xoè rộng, màusắc tươi sáng thể hiện sức sống mạnh liệt, sư nhiệt tình tuyệt vời.</p>
 
-        <p>Đặc biệt, hoa đồng tiền có thể nở quanh năm. Do câu dễ trồng và chăm sóc nên chỉ cần tưới nước cho cây 1 – 2 lần là cây có thể cho hoa.</p>
+        <p class="p">Đặc biệt, hoa đồng tiền có thể nở quanh năm. Do câu dễ trồng và chăm sóc nên chỉ cần tưới nước cho cây 1 – 2 lần là cây có thể cho hoa.</p>
 
 
         <div class="image-flower" align="center" style="margin-bottom: 30px;">
             <img src="{{asset('../public/img/yNghia/hoadongtien1.png')}}" alt="" width="75%" />
-            <p style="text-align: center"><i>Đồng tiền trắng tượng trưng cho sự trong trắng tinh khiết</i></p>
+            <p style="text-align: center" class="p"><i>Đồng tiền trắng tượng trưng cho sự trong trắng tinh khiết</i></p>
         </div>
 
         <div class="image-flower" align="center" style="margin-bottom: 30px;">
             <img src="{{asset('../public/img/yNghia/hoadongtien2.png')}}" alt="" width="75%" />
-            <p style="text-align: center"><i>Tình yêu ở những bông hoa màu đỏ</i></p>
+            <p style="text-align: center" class="p"><i>Tình yêu ở những bông hoa màu đỏ</i></p>
         </div>
 
         <div class="image-flower" align="center" style="margin-bottom: 30px;">
             <img src="{{asset('../public/img/yNghia/hoadongtien3.png')}}" alt="" width="75%" />
-            <p style="text-align: center"> <i>Niềm hạnh phúc, sung túc của hoa màu vàng</i></p>
+            <p style="text-align: center" class="p"> <i>Niềm hạnh phúc, sung túc của hoa màu vàng</i></p>
         </div>
 
         <div class="image-flower" align="center" style="margin-bottom: 30px;">
             <img src="{{asset('../public/img/yNghia/hoadongtien4.png')}}" alt="" width="75%" />
-            <p style="text-align: center"><i>Rạng rỡ tươi vui với sắc cam</i></p>
+            <p style="text-align: center" class="p"><i>Rạng rỡ tươi vui với sắc cam</i></p>
         </div>
 
         <div class="image-flower" align="center" style="margin-bottom: 30px;">
             <img src="{{asset('../public/img/yNghia/hoadongtien5.png')}}" alt="" width="75%" />
-            <p style="text-align: center"> <i>sự ca ngợi khích lệ của màu hồng</i></p>
+            <p style="text-align: center" class="p"> <i>sự ca ngợi khích lệ của màu hồng</i></p>
         </div>
 
 @endsection
--- resources/views/page/ynghiahoa/HoaHuongDuong.blade.php
+++ resources/views/page/ynghiahoa/HoaHuongDuong.blade.php
@@ -9,44 +9,44 @@
     <h3>Ý nghĩa hoa hướng dương</h3>
     <div align="center"><iframe width="580" height="335" src="https://www.youtube.com/embed/yfvZwpmkSPI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
     <div class="text-here">
-        <p><strong>
+        <p class="p"><strong>
                 Thế Giới Hoa Tươi cùng bạn tham khảo ý nghĩa một số loài hoa với mong muốn bạn sẽ chọn được những bó hoa tươi ưng ý, phù hợp với từng dịp lễ và tình cảm của mình. Dù thế nào đi nữa người nhận sẽ hạnh phúc khi nhận được tình cảm chân thành của bạn muốn
                 nhờ những bông hoa thay lời nói.</strong></p>
-        <p>+ Hoa hướng dương tượng trưng cho sự tôn thờ, lòng trung thành và sự trường thọ. Phần lớn ý nghĩa của hoa hướng dương bắt nguồn từ tên gọi của nó, không đâu khác chính là …”Mặt trời”.</p>
+        <p class="p">+ Hoa hướng dương tượng trưng cho sự tôn thờ, lòng trung thành và sự trường thọ. Phần lớn ý nghĩa của hoa hướng dương bắt nguồn từ tên gọi của nó, không đâu khác chính là …”Mặt trời”.</p>
 
         Những bông hoa này là độc nhất, chính ở chỗ chúng có khả năng cung cấp năng lượng dưới hình thức dinh dưỡng. Và tính chất sống động phản chiếu ánh nắng mặt trời và năng lượng do nhiệt và ánh sáng tạo ra.</p>
 
 
 
-        <p>+ Khiến cho nhiều người đôi khi nhìn thấy những bông hoa hướng dương thường xuyên chăm chú nhìn về phía ánh mặt trời ấm áp</p>
+        <p class="p">+ Khiến cho nhiều người đôi khi nhìn thấy những bông hoa hướng dương thường xuyên chăm chú nhìn về phía ánh mặt trời ấm áp</p>
 
-        <p>+ Những đóa hoa mặt trời được biết đến là biểu tượng của niềm vui, niềm hạnh phúc.</p>
+        <p class="p">+ Những đóa hoa mặt trời được biết đến là biểu tượng của niềm vui, niềm hạnh phúc.</p>
 
-        <p>+ Khiến chúng trở thành một trong những món quà hoàn hảo để “biến” một ngày của “ai đó” trở nên đầy ý nghĩa. Tràn ngập năng lượng và niềm vui trong cuộc sống</p>
+        <p class="p">+ Khiến chúng trở thành một trong những món quà hoàn hảo để “biến” một ngày của “ai đó” trở nên đầy ý nghĩa. Tràn ngập năng lượng và niềm vui trong cuộc sống</p>
         <div class="image-flower" align="center">
             <img src="{{asset('../public/img/yNghia/hoahuongduong1.png')}}" alt="" width="75%" />
         </div>
-        <p> <strong><big>Ý nghĩa của hoa hướng dương trong cuộc sống</big></strong></p>
-        <p><strong><strong><strong>May mắn và hạnh phúc </strong></strong></strong></p>
-        <p>Hoa hướng dương mang một màu vàng ấm áp tạo một không khí tươi vui tràn đầy sức sống. Đem lại sự may mắn và hạnh phúc trong cuộc sống, thường được dùng trong các dịp khai trương chúc mừng, các buổi tiệc sinh nhật. </p>
-        <p><strong><strong><strong>Sức khỏe và trường thọ</strong></strong></strong> </p>
-        <p>Loài hoa mang đặc tính hướng quang nên luôn luôn hướng về phía mặt trời thể hiện một sức sống mãnh liệt. Sự trường thọ, luôn hướng về phía trước và vươn lên trong cuộc sống. </p>
-        <p><strong><strong><strong>Sự tôn kính và biết ơn </strong></strong></strong></p>
-        <p>Hoa hướng dương còn được xem là loài hoa biểu hiện sự tôn kính và biết ơn với người thân. Nên bạn có thể tặng cho ông bà, cha mẹ vào những ngày đặc biệt như sinh nhật, ngày của cha, ngày 20/10,… </p>
-        <p><strong><strong><strong>Sự lạc quan, mạnh mẽ, tương lai tươi sáng</strong></strong></strong></p>
-        <p>Hoa hướng dương vượt qua sự khắc nghiệt của thời tiết luôn hướng về phía mặt trời. </p>
-        <p>Nếu bạn muốn cổ vũ tinh thần lạc quan, mạnh mẽ vươn lên trong cuộc sống và phấn chấn làm việc cho một ai đó.  Thì hoa hướng dương rất thích hợp nhất lúc này. </p>
+        <p class="p"> <strong><big>Ý nghĩa của hoa hướng dương trong cuộc sống</big></strong></p>
+        <p class="p"><strong><strong><strong>May mắn và hạnh phúc </strong></strong></strong></p>
+        <p class="p">Hoa hướng dương mang một màu vàng ấm áp tạo một không khí tươi vui tràn đầy sức sống. Đem lại sự may mắn và hạnh phúc trong cuộc sống, thường được dùng trong các dịp khai trương chúc mừng, các buổi tiệc sinh nhật. </p>
+        <p class="p"><strong><strong><strong>Sức khỏe và trường thọ</strong></strong></strong> </p>
+        <p class="p">Loài hoa mang đặc tính hướng quang nên luôn luôn hướng về phía mặt trời thể hiện một sức sống mãnh liệt. Sự trường thọ, luôn hướng về phía trước và vươn lên trong cuộc sống. </p>
+        <p class="p"><strong><strong><strong>Sự tôn kính và biết ơn </strong></strong></strong></p>
+        <p class="p">Hoa hướng dương còn được xem là loài hoa biểu hiện sự tôn kính và biết ơn với người thân. Nên bạn có thể tặng cho ông bà, cha mẹ vào những ngày đặc biệt như sinh nhật, ngày của cha, ngày 20/10,… </p>
+        <p class="p"><strong><strong><strong>Sự lạc quan, mạnh mẽ, tương lai tươi sáng</strong></strong></strong></p>
+        <p class="p">Hoa hướng dương vượt qua sự khắc nghiệt của thời tiết luôn hướng về phía mặt trời. </p>
+        <p class="p">Nếu bạn muốn cổ vũ tinh thần lạc quan, mạnh mẽ vươn lên trong cuộc sống và phấn chấn làm việc cho một ai đó.  Thì hoa hướng dương rất thích hợp nhất lúc này. </p>
         <div class="image-flower" align="center">
             <img src="{{asset('../public/img/yNghia/hoahuongduong2.png')}}" alt="" width="75%" />
         </div>
-        <p><strong><strong><strong>Ý nghĩa của hoa hướng dương trong tình bạn </strong></strong></strong></p>
-        <p>Đó là sự chân thành, tin tưởng, đoàn kết giữa con người với con người. Vì vậy loài hoa này thường được dùng trong các buổi hợp tác làm ăn của công ty hay các tổ chức nào đó. </p>
-        <p><strong><strong><strong>Ý nghĩa của hoa hướng dương trong ngày Tết </strong></strong></strong></p>
-        <p>Hoa hướng dương đại diện cho niềm vui, sự ấm no, hạnh phúc trong cuộc sống. Nên được trưng bày rất nhiều trong ngày Tết và mọi người thường hay lấy loài hoa này làm quà tặng.</p>
-        <p><strong><strong><strong>Ý nghĩa của hoa hướng dương trong ngày cưới</strong></strong></strong> </p>
-        <p>+ Trong tình yêu, hoa hướng dương đem lại sự chung thủy, chân thành. Cho dù yêu xa hay ở trong hoàn cảnh nào vẫn luôn nhớ về nhau </p>
-        <p>+ Nên thường được trang trí trong các lễ cưới hỏi, làm hoa cưới cầm tay cho cô dâu. Với mong muốn tình yêu của họ luôn luôn bền vững, hạnh phúc mãi mãi. </p>
-        <p>Loài hoa hướng dương này đem đến vô cùng nhiều ý nghĩa cao cả. Không chỉ một phương diện nào đó mà là nhiều phương diện khác nhau. </p>
+        <p class="p"><strong><strong><strong>Ý nghĩa của hoa hướng dương trong tình bạn </strong></strong></strong></p>
+        <p class="p">Đó là sự chân thành, tin tưởng, đoàn kết giữa con người với con người. Vì vậy loài hoa này thường được dùng trong các buổi hợp tác làm ăn của công ty hay các tổ chức nào đó. </p>
+        <p class="p"><strong><strong><strong>Ý nghĩa của hoa hướng dương trong ngày Tết </strong></strong></strong></p>
+        <p class="p">Hoa hướng dương đại diện cho niềm vui, sự ấm no, hạnh phúc trong cuộc sống. Nên được trưng bày rất nhiều trong ngày Tết và mọi người thường hay lấy loài hoa này làm quà tặng.</p>
+        <p class="p"><strong><strong><strong>Ý nghĩa của hoa hướng dương trong ngày cưới</strong></strong></strong> </p>
+        <p class="p">+ Trong tình yêu, hoa hướng dương đem lại sự chung thủy, chân thành. Cho dù yêu xa hay ở trong hoàn cảnh nào vẫn luôn nhớ về nhau </p>
+        <p class="p">+ Nên thường được trang trí trong các lễ cưới hỏi, làm hoa cưới cầm tay cho cô dâu. Với mong muốn tình yêu của họ luôn luôn bền vững, hạnh phúc mãi mãi. </p>
+        <p class="p">Loài hoa hướng dương này đem đến vô cùng nhiều ý nghĩa cao cả. Không chỉ một phương diện nào đó mà là nhiều phương diện khác nhau. </p>
 
     </div>
 
