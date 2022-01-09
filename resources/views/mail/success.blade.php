013b7d67b55c86c4d9e7f3976488a6aaa44d7b83
tuankha2021 <79949413+tuankha2021@users.noreply.github.com>
2022-01-09 10:18:18 +0700

    Add files via upload

--- resources/views/mail/success.blade.php
+++ resources/views/mail/success.blade.php
@@ -1,1 +1,6 @@
-<h1>Bạn có đơn hàng mới </h1>
+<div style=" text-align: center; background: rgb(7,255,252);
+background: linear-gradient(30deg, rgba(7,255,252,0.8886905103838411) 2%, rgba(143,255,2,0.8578781854538691) 37%, rgba(182,227,51,0.7654412106639531) 68%, rgba(58,252,7,0.6786064767703957) 80%, rgba(0,212,255,0.3508753843334209) 100%);">
+    <h1 >Flower Shop thông báo </h1>
+    <h3>Bạn có đơn đặc hàng mới </h3>
+    <p> vui lòng truy cập trang web để xem <a href="{{asset('../FlowerShop/public/admin/menu/DSDonHang')}}">thông tin chi tiết</a></p>
+</div>
